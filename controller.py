fe2f09754ba7f52e59d2d1ce6786877911bcacc4
fedotarte <artyom.fedotov93@gmail.com>
2019-04-05 22:21:50 +0300

    added handle for string commands

--- controller.py
+++ controller.py
@@ -1,4 +1,8 @@
+import os
 import sys
+
+import psutil
+import logging
 
 from microwave import Microwave
 
@@ -9,6 +13,12 @@ class Controller:
     microwave = None
 
     def __init__(self):
+
+        self.string_command_types = {
+            'Q': self.quit_microwave,
+            'R': self.quit_microwave
+        }
+
         self.int_command_types = {
             0: self.init_the_microwave,
             1: self.switch_on_off,
@@ -21,6 +31,11 @@ class Controller:
         }
 
     # check the input is digit
+    def get_str_commands(self, str_input):
+        for key, value in self.int_command_types.items():
+            if key == str_input:
+                value()
+
     def get_choice_in_game(self, int_input):
         for key, value in self.int_command_types.items():
             if key == int_input:
@@ -33,9 +48,12 @@ class Controller:
 
         except Exception as e:
             print("you got an exception:" + str(e))
-
+            try:
+                self.get_str_commands(inputed_text)
+            except Exception as ex :
+                print("the command is  uncorrect! " + str(ex))
         finally:
-            print("to quit press q, t orestart r")
+            print("to quit press q/Q, to  restart press r/R")
 
     # running through the enum "int_command_types" to find the correct key and initialize the related method
     # in the main.py : get_choice_in_game(check_correct_digit(inputed_text))
@@ -75,6 +93,17 @@ class Controller:
     def microwave_exists(self, check_microwave):
         return self.microwave == check_microwave
 
-    def quit(self, inputed_text):
-        if self.inputed_text.to_upper_case == "Q":
+    def restart_microwave(self, inputed_text):
+        try:
+            p = psutil.Process(os.getpid())
+            for handler in p.get_open_files() + p.connections():
+                os.close(handler.fd)
+        except Exception as e:
+            logging.error(e)
+
+        python = sys.executable
+        os.execl(python, python, *sys.argv)
+
+    def quit_microwave(self, inputed_text):
+        if self.inputed_text.to_upper_case == "Q":  # TODO without if
             exit()
